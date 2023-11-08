65a0f6a2b011e107c6099c357751ef83c9cc95e6
ashok <ashokashi1997@gmail.com>
2023-11-08 16:52:16 +0530

    BILLING

---
+++ src/com/billing/demo_three.java
@@ -0,0 +1,11 @@
+package com.billing;
+
+public class demo_three {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("/Karnataka electricity billing systemS");
+	}
+
+}
